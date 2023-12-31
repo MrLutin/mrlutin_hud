efa43b50ba7866267b619b00997659eca21944be
Mr.Lutin <mcotemw@icloud.com>
2023-12-31 14:18:08 -0500

    Add missing id for the icon to be trigger by javascript

--- client/seatbelt.lua
+++ client/seatbelt.lua
@@ -1,16 +1,35 @@
+local hasSeatbelt = false
+local hasHarnest = false
+
+RegisterKeyMapping('seatbelt', 'Put/Take off Seatbelt', 'keyboard', 'b')
+RegisterCommand('seatbelt', function()
+    hasSeatbelt = not hasSeatbelt
+
+    if hasSeatbelt then
+        SendMessage('setSeatbelt', { toggled = true, buckled = true })
+        SetFlyThroughWindscreenParams(120.0, 120.0, 0.0, 0.0)
+    elseif hasHarnest then
+        SendMessage('setSeatbelt', { toggled = true, buckled = true })
+        SetFlyThroughWindscreenParams(1000.0, 1000.0, 0.0, 0.0)
+    else
+        SendMessage('setSeatbelt', { toggled = true, buckled = false })
+        ResetFlyThroughWindscreenParams()
+    end
+end, false)
 
 -- Global Seatbel thread
 CreateThread(function()
     while true do
-        if cache.vehicle then
+        if cache.vehicle and ( hasSeatbelt or hasHarnest ) then
             local model = GetEntityModel(cache.vehicle)
             local currentSpeed = GetEntitySpeed(cache.vehicle)
             local isMetric = ShouldUseMetricMeasurements()
             local units = isMetric and 3.6 or 2.236936
             local nitrous = Entity(cache.vehicle).state.nitrous
-            SendMessage('setSeatbelt', { toggled = true, buckled = true })
+
         elseif not cache.vehicle then
-            SendMessage('setSeatbelt', { toggled = true, buckled = false })
+            hasSeatbelt = false
+            hasHarnest = false
         end
         Wait(200)
     end
